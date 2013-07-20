ad531aee3bdc259e21157534dd729e425b9c5401
Yann Hamon <yann@mandragor.org>
2013-07-20 14:59:41 +0200

    Added route centroid calculation + cleaner SQL GIS queries

--- lib/databases/postgis.php
+++ lib/databases/postgis.php
@@ -8,8 +8,28 @@ class Postgis
 	extends \PDO 
 	implements \TB\iDatabase {
 
+
   function __construct($dsn, $username="", $password="", $driver_options=array()) {
       parent::__construct($dsn,$username,$password, $driver_options);
+  }
+
+
+  function updateRouteCentroid($routeid) {
+		$this->beginTransaction();
+		$q = "UPDATE routes 
+          SET center = (
+              SELECT  ST_AsBinary(ST_Centroid(ST_MakeLine(rp.coords ORDER BY rp.pointnumber ASC)))
+              FROM routepoints rp
+              WHERE routes.id = rp.routeid )
+          WHERE id=?;";
+		$pq = $this->prepare($q);
+		$success = $pq->execute(array($routeid));
+    if (!$success) {
+      $this->rollBack();
+      throw (new tbApiException("Failed to insert the track into the database - Problem calculating centroid", 500));
+    }
+
+		$this->commit();
   }
 
 	function importRoute($route) {
@@ -19,7 +39,10 @@ class Postgis
 		$q = "INSERT INTO routes (name) VALUES (?)";
 		$pq = $this->prepare($q);
 		$success = $pq->execute(array("random"));
-		if (!$success) {echo "FAILED TO INSERT:".print_r($pq->errorInfo())."<br>";}
+    if (!$success) {
+      $this->rollBack();
+      throw (new tbApiException("Failed to insert the track into the database", 500));
+    }
 
     $routeid = intval($this->lastInsertId("routes_id_seq"));
 
@@ -49,11 +72,13 @@ class Postgis
 				$tags,
 			));
 			if (!$success) {
+        $this->rollBack();
 				throw (new tbApiException("Failed to insert the track into the database", 500));
 			}
 		}
-
 		$this->commit();
+    $this->updateRouteCentroid($routeid);
+
 		return $routeid;
 	}
 
@@ -61,22 +86,14 @@ class Postgis
 
 	function exportRoute($routeid, $format) {
 		$this->beginTransaction();
-		$q = "SELECT route.id AS routeid, 
-		    	  route.name AS name, 
-					  ST_AsGeoJson(ST_MakeLine(routepoint.coords)) AS route
-					FROM routes as route,
-			  		( SELECT point.routeid,
-			      		point.pointnumber,
-			      		point.coords
-			   			FROM routepoints as point
-			   			WHERE routeid=?
-			   			ORDER BY pointnumber ASC)
-			   	AS routepoint
-					WHERE route.id=?
-					GROUP BY route.id, routepoint.routeid";
-
+		$q = "SELECT r.id AS routeid, 
+                 r.name AS name, 
+                 ST_AsGeoJson(ST_MakeLine(rp.coords ORDER BY rp.pointnumber ASC)) AS line 
+          FROM routes r, routepoints rp
+          WHERE r.id=? AND rp.routeid=r.id
+          GROUP BY r.id";
 		$pq = $this->prepare($q);
-		$success = $pq->execute(array($routeid,$routeid));
+		$success = $pq->execute(array($routeid));
 		if (!$success) {
 			throw (new tbApiException("Failed to fetch route from Database", 500));
 		}
