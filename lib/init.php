08086db125742dc8c86aab482b46ecda534ff9ce
Yann Hamon <yann@trailburning.com>
2013-07-30 15:45:39 +0200

    Doc_root seems to miss trailing slash on heroku

--- lib/init.php
+++ lib/init.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 $api_root = $_SERVER["DOCUMENT_ROOT"];
 $conf_path = $api_root.'/config/';
 
-$include_path = get_include_path().PATH_SEPARATOR.$api_root.'lib/'.PATH_SEPARATOR.$api_root.'vendor/';
+$include_path = get_include_path().PATH_SEPARATOR.$api_root.'/lib/'.PATH_SEPARATOR.$api_root.'/vendor/';
 set_include_path($include_path);
 
 ?>
