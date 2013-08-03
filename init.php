d9e1148037c74f021c238a04fe3c59515d95324b
Yann Hamon <yann@trailburning.com>
2013-08-03 16:38:20 +0200

    sanitize include path and add /templates

--- init.php
+++ init.php
@@ -7,7 +7,10 @@ error_reporting(E_ALL);
 $api_root = isset($_SERVER["DOCUMENT_ROOT"])?'.':$_SERVER["DOCUMENT_ROOT"];;
 $conf_path = $api_root.'/config/';
 
-$include_path = get_include_path().PATH_SEPARATOR.$api_root.'/lib/'.PATH_SEPARATOR.$api_root.'/vendor/';
+$include_path = get_include_path();
+$include_path .= PATH_SEPARATOR.$api_root.'/lib/';
+$include_path .= PATH_SEPARATOR.$api_root.'/vendor/';
+$include_path .= PATH_SEPARATOR.$api_root.'/templates/';
 set_include_path($include_path);
 
 // Strtotime should read datetime without timezone info as relative to UTC, and not current
