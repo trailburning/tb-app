3d88b29f399eae137528c55c11609239cc570a00
Yann Hamon <yann@trailburning.com>
2013-08-06 03:33:55 +0200

    Heroku debugging

--- templates/ApiReplyView.php
+++ templates/ApiReplyView.php
@@ -1,5 +1,6 @@
 <?php
   $params = array();
+  echo "TOTO:".array_key_exists('usermsg', $this->data);
   if (array_key_exists('usermsg', $this->data) == TRUE) $params['usermsg'] = $this->data['usermsg'];
   if (array_key_exists('debugmsg', $this->data) == TRUE) $params['debugmsg'] = $this->data['debugmsg'];
   if (array_key_exists('value', $this->data) == TRUE) $params['value'] = json_decode($this->data['value']);
