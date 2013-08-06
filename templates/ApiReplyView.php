670d66396cb46194bb34c187c51da9e93f7415ad
Yann Hamon <yann@trailburning.com>
2013-08-06 03:28:12 +0200

    Heroku debugging

--- templates/ApiReplyView.php
+++ templates/ApiReplyView.php
@@ -1,9 +1,8 @@
 <?php
   $params = array();
-  $params['usermsg'] = $this->data['usermsg'];
-  if (array_key_exists('debugmsg', $this->data)) $params['debugmsg'] = $this->data['debugmsg'];
-  $params['value'] = json_decode($this->data['value']);
-  $params['test'] = "COIN";
+  if (array_key_exists('usermsg', $this->data) === TRUE) $params['usermsg'] = $this->data['usermsg'];
+  if (array_key_exists('debugmsg', $this->data) === TRUE) $params['debugmsg'] = $this->data['debugmsg'];
+  if (array_key_exists('value', $this->data)=== TRUE) $params['value'] = json_decode($this->data['value']);
 
   echo json_encode($params);
 ?>
