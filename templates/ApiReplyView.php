7f95c505fac512ca4dad3b925e8c8ee07f2c2928
Yann Hamon <yann@trailburning.com>
2013-08-06 03:39:26 +0200

    Heroku debugging

--- templates/ApiReplyView.php
+++ templates/ApiReplyView.php
@@ -1,8 +1,11 @@
 <?php
   $params = array();
+  
+  // This array_key_exists doesn't work on heroku. It doesn't make sense :(
+  // if (array_key_exists('usermsg', $this->data)) $params['usermsg'] = $this->data['usermsg'];
   if (isset($this->data['usermsg'])) $params['usermsg'] = $this->data['usermsg'];
-  if (array_key_exists('debugmsg', $this->data) == TRUE) $params['debugmsg'] = $this->data['debugmsg'];
-  if (array_key_exists('value', $this->data) == TRUE) $params['value'] = json_decode($this->data['value']);
+  if (isset($this->data['debugmsg'])) $params['debugmsg'] = $this->data['debugmsg'];
+  if (isset($this->data['value'])) $params['value'] = json_decode($this->data['value']);
 
   echo json_encode($params);
 ?>
