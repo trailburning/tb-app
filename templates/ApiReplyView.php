18809ed200e8a0b619498b63b356d1c022a821e1
Yann Hamon <yann@trailburning.com>
2013-08-06 03:04:03 +0200

    Test for heroku bug

--- templates/ApiReplyView.php
+++ templates/ApiReplyView.php
@@ -1,9 +1,9 @@
 <?php
-
   $params = array();
   if (array_key_exists('usermsg', $this->data)) $params['usermsg'] = $this->data['usermsg'];
   if (array_key_exists('debugmsg', $this->data)) $params['debugmsg'] = $this->data['debugmsg'];
   if (array_key_exists('value', $this->data)) $params['value'] = json_decode($this->data['value']);
+  $params['test'] = "COIN";
 
   echo json_encode($params);
 ?>
