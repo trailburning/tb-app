353e29ee720abd404259666d83133074330f6dde
Yann Hamon <yann@trailburning.com>
2013-08-07 05:10:08 +0200

    Better test message for non existing routes

--- tests/tb-restapi/ExportNonExistingRouteCept.php
+++ tests/tb-restapi/ExportNonExistingRouteCept.php
@@ -2,7 +2,7 @@
 use \ApiGuy;
 
 $I = new ApiGuy($scenario);
-$I->wantTo('Try to export a route that doesn\'t exist');
+$I->wantTo('export a route that doesn\'t exist');
 $I->SendGet('/v1/route/999999');
 $I->seeResponseCodeIs(404);
 $I->seeResponseIsJson();
