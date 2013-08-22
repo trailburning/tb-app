ad5b1081ed885d39f7214af3891c04b87fe0e83a
Yann Hamon <yann@trailburning.com>
2013-08-22 12:08:51 +0200

    Allow CORS DELETE requests

--- init.php
+++ init.php
@@ -13,7 +13,8 @@ $include_path .= PATH_SEPARATOR.$api_root.'/templates/';
 set_include_path($include_path);
 
 // Allow third party sites to make AJAX calls to this API
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
 
 // Strtotime should read datetime without timezone info as relative to UTC, and not current
 // server timezone
