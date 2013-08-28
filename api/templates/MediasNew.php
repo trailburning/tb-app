ed7e66dfce6395cdfb64f86298d60af8327a2258
Yann Hamon <yann@trailburning.com>
2013-08-28 15:42:52 +0200

    Updated mockup forms as api now in its own folder

--- api/templates/MediasNew.php
+++ api/templates/MediasNew.php
@@ -1,4 +1,4 @@
-<form action="/v1/route/<?php echo $routeid; ?>/medias/add" method="post" enctype="multipart/form-data">
+<form action="/api//v1/route/<?php echo $routeid; ?>/medias/add" method="post" enctype="multipart/form-data">
   <input type="file" name="medias[]" multiple>
   <input type="submit">
 </form>
