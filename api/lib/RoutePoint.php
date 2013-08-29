0845c7d704abbba41dbe12cf66d979acf79a8709
Yann Hamon <yann@trailburning.com>
2013-08-29 12:06:18 +0200

    Assign altitude to pictures from routepoint

--- api/lib/RoutePoint.php
+++ api/lib/RoutePoint.php
@@ -16,6 +16,13 @@ class RoutePoint {
     return $this->tags;
   }
 
+  public function getTag($tag) {
+    if (array_key_exists($tag, $this->tags))
+      return $this->tags[$tag];
+    else
+      return null;
+  }
+
   public function getCoords() {
     return $this->coords;
   }
