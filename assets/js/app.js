4cdad1b4bf645de5314d461c6d7a49f276501e73
Matt Allbeury <mallbeury@mac.com>
2013-09-03 16:05:49 +0200

    Updated altitude view to contain background lines and main image view to be full width.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,29 +3,41 @@ var app = app || {};
 var RESTAPI_BASEURL = 'http://trailburning.herokuapp.com/api/';
 //var RESTAPI_BASEURL = 'http://localhost:8888/api/';
 
+//https://s3-eu-west-1.amazonaws.com/trailburning-media/af86ca5c0885dfda4a174aa5ed08a33d408087e0.jpg
+
 define([
   'underscore', 
   'backbone',
   'models/TrailModel',
+  'models/TrailMediaModel',
   'views/TrailMapView',
   'views/TrailAltitudeView'  
-], function(_, Backbone, TrailModel, TrailMapView, TrailAltitudeView){
+], function(_, Backbone, TrailModel, TrailMediaModel, TrailMapView, TrailAltitudeView){
   app.dispatcher = _.clone(Backbone.Events);
   
   var initialize = function() {
     var self = this;
     
-    var trailModel = new TrailModel();
+    this.trailModel = new TrailModel();
+    this.mediaModel = new TrailMediaModel();
 
-    this.trailMapView = new TrailMapView({ el: '#trailmapview', model: trailModel });
-    this.trailAltitudeView = new TrailAltitudeView({ el: '#trailaltitudeview', model: trailModel });
+    this.trailMapView = new TrailMapView({ el: '#trailmapview', model: this.trailModel });
+    this.trailAltitudeView = new TrailAltitudeView({ el: '#trailaltitudeview', model: this.trailModel });
     
     $(window).resize(function() {
       handleResize(); 
     });    
     handleResize();        
     
-    function handleResize() {      
+    function handleResize() {
+      var nDetailContainerWidth = $('.detail1_container').width();
+      var nPlayerPanelWidth = $(window).width() - nDetailContainerWidth - 20;
+      if (nPlayerPanelWidth < 620) {
+        nPlayerPanelWidth = 620;
+      } 
+      
+      $('.trailplayer_panel').width(nPlayerPanelWidth);
+      $('.trailplayer_panel .imageContainer').width(nPlayerPanelWidth - 10);      
       $('.image').resizeToParent();
             
       self.trailMapView.update();
@@ -34,15 +46,31 @@ define([
       $('.image').show();
     }        
     
+    function handleMedia() {
+      var data = self.mediaModel.get('value');      
+      $.each(data, function(key, point) {
+        console.log('t:'+point.coords.lat+' : '+point.coords.long);
+        self.trailAltitudeView.addMediaMarker(point.coords.lat, point.coords.long);        
+      });
+    }
+    
     // get trail    
-//    trailModel.set('id', 6);    
-    trailModel.set('id', 14);    
-    console.log('Fetch ID:'+trailModel.get('id'));            
-    trailModel.fetch({
+//    this.trailModel.set('id', 2);    
+    this.trailModel.set('id', 15);    
+    console.log('Fetch ID:'+this.trailModel.get('id'));            
+    this.trailModel.fetch({
       success: function () {
         console.log('Fetched');
         self.trailMapView.render();
         self.trailAltitudeView.render();
+        
+        self.mediaModel.url = RESTAPI_BASEURL + 'v1/route/'+self.trailModel.get('id')+'/medias';
+        self.mediaModel.fetch({
+          success: function () {
+            console.log('Fetched media');
+            handleMedia(self.mediaModel);
+          }
+        });        
       }      
     });    
   };
--- assets/js/views/TrailAltitudeView.js
+++ assets/js/views/TrailAltitudeView.js
@@ -7,12 +7,39 @@ define([
     initialize: function(){
       this.template = _.template($('#traiAltitudeViewTemplate').text());        
             
-      this.HeightToWidthFactor = 4;
-      
+      this.HeightToWidthFactor = 4;      
       this.fXFactor = 0;
-      this.fYFactor = 0;
-
       this.jsonTrail = null;
+      this.fLowAlt = 0;
+      this.fHighAlt = 0;
+      
+      this.objTrailMarginRect = new Object();
+      this.objTrailMarginRect.left = 50;
+      this.objTrailMarginRect.right = 50;
+      this.objTrailMarginRect.top = 20;
+      this.objTrailMarginRect.bottom = 20;
+      
+      this.objBackgroundMarginRect = new Object();
+      this.objBackgroundMarginRect.left = 50;
+      this.objBackgroundMarginRect.right = 50;
+      this.objBackgroundMarginRect.top = 20;
+      this.objBackgroundMarginRect.bottom = 60;
+      
+      CanvasRenderingContext2D.prototype.dashedVerticalLine = function(nX, nY1, nY2, nDashStrokeHeight) {
+        if (nDashStrokeHeight == undefined) nDashStrokeHeight = 2;
+
+        var nDashHeight = 7;
+        var nDashes = Math.floor(nY2 - nY1) / nDashHeight;
+        
+        var nDash = 0;
+        var nCurrY = nY1;
+        while (nDash++ < nDashes) {
+          this.moveTo(nX, nCurrY);            
+          this.lineTo(nX, nCurrY + nDashStrokeHeight);
+          
+          nCurrY += nDashHeight;
+        }
+      };      
     },            
     render: function(fScale){
       console.log('TrailAltitudeView:render');
@@ -34,10 +61,8 @@ define([
                   
       this.canvas = this.elCanvas[0];
 
-      this.nCanvasWidth = ($(this.el).width() * 90) / 100;
-      this.nCanvasHeight = ($(this.el).height() * 60) / 100;;
-      this.elCanvas.css('top', ($(this.el).height() * 40) / 100);
-      this.elCanvas.css('left', ($(this.el).width() * 5) / 100);
+      this.nCanvasWidth = $(this.el).width();
+      this.nCanvasHeight = $(this.el).height();
 
       this.elCanvas.attr('width', this.nCanvasWidth);
       this.elCanvas.attr('height', this.nCanvasHeight);
@@ -49,12 +74,11 @@ define([
       this.nDrawWidth = this.nCanvasWidth;
       this.nDrawHeight = this.nCanvasHeight;
             
-      var data = this.model.get('value');            
+      var data = this.model.get('value');
+      
+      this.updateValues(data.route.route_points, Number(data.route.length));      
+      this.renderBackground(Number(data.route.length));                  
       this.renderTrail(data.route.route_points, Number(data.route.length));
-
-      this.context.lineWidth = 4;
-      this.context.strokeStyle = 'rgba(68,182,252,1)';
-      this.context.stroke();
 
       return this;
     },
@@ -62,65 +86,87 @@ define([
       if (this.jsonTrail) {
         this.render();      
       }
-    },
-    renderTrail: function(jsonPoints, fTrailLengthMetres) {
-      this.jsonTrail = jsonPoints;
-            
-       // find highest/lowest points
+    },    
+    updateValues: function(jsonPoints, fTrailLengthMetres) {
+      var self = this;
+      
+      this.nCanvasDrawWidth = $(this.el).width() - (this.objTrailMarginRect.left + this.objTrailMarginRect.right);
+      this.nCanvasDrawHeight = $(this.el).height() - (this.objTrailMarginRect.top + this.objTrailMarginRect.bottom);
+      
+      // find highest/lowest points
       if (jsonPoints.length) {
-        fLowAlt = jsonPoints[0].tags.altitude;
-        fHighAlt = jsonPoints[0].tags.altitude;
+        this.fLowAlt = Number(jsonPoints[0].tags.altitude);
+        this.fHighAlt = Number(jsonPoints[0].tags.altitude);
       }
       $.each(jsonPoints, function(key, point) {
-        if (Number(point.tags.altitude) < fLowAlt) {
-          fLowAlt = Number(point.tags.altitude); 
+        if (Number(point.tags.altitude) < self.fLowAlt) {
+          self.fLowAlt = Number(point.tags.altitude); 
         }
-        if (Number(point.tags.altitude) > fHighAlt) {
-          fHighAlt = Number(point.tags.altitude); 
+        if (Number(point.tags.altitude) > self.fHighAlt) {
+          self.fHighAlt = Number(point.tags.altitude); 
         }    
       });     
       
-      var fRange = fHighAlt - fLowAlt;
+      this.fAltRange = this.fHighAlt - this.fLowAlt;
       
       var fProfileWidthMetres = fTrailLengthMetres;
-      var fProfileHeightMetres = fRange * this.HeightToWidthFactor;
+      var fProfileHeightMetres = this.fAltRange * this.HeightToWidthFactor;
         
       var nRatio = Math.round(fProfileWidthMetres / fProfileHeightMetres); 
 
       // adjust draw width
       this.nDrawWidth = this.nDrawHeight * nRatio;
       // scale to fit canvas        
-      if (this.nDrawWidth > this.nCanvasWidth || this.nDrawHeight > this.nCanvasHeight) {
-        this.nDrawHeight = Math.round(this.nDrawHeight * this.nCanvasWidth / this.nDrawWidth);
-        this.nDrawWidth = this.nCanvasWidth
+      if (this.nDrawWidth > this.nCanvasDrawWidth || this.nDrawHeight > this.nCanvasDrawHeight) {
+        this.nDrawHeight = Math.round(this.nDrawHeight * this.nCanvasDrawWidth / this.nDrawWidth);
+        this.nDrawWidth = this.nCanvasDrawWidth;
       }  
+    },    
+    renderBackground: function(fTrailLengthMetres) {
+      this.context.beginPath();
+      
+      var nMarkers = fTrailLengthMetres / 1000;
+      var nMarkerWidth = this.nDrawWidth / nMarkers;
 
+      var nX = this.objBackgroundMarginRect.left;
+      for (var nMarker=0; nMarker <= nMarkers; nMarker++) {
+        this.context.dashedVerticalLine(Math.round(nX), this.objBackgroundMarginRect.top, this.nCanvasHeight - this.objBackgroundMarginRect.bottom, 2);
+        nX += nMarkerWidth;
+      }
+      
+      this.context.lineWidth = 1;
+      this.context.strokeStyle = 'rgba(68,182,252,1)';
+      this.context.stroke();
+    },    
+    renderTrail: function(jsonPoints, fTrailLengthMetres) {
+      this.jsonTrail = jsonPoints;
+                              
       this.fXFactor = jsonPoints.length / this.nDrawWidth;
-      this.fYFactor = fHighAlt / this.nDrawHeight;
 
       var self = this;
-      var nXOffset = (this.nCanvasWidth - this.nDrawWidth) / 2;
+      var nXOffset = (this.nCanvasDrawWidth - this.nDrawWidth) / 2;
+      var nYOffset = (this.nCanvasDrawHeight - this.nDrawHeight) / 2;
+            
       var nX = 0;
       var nY = 0;
       var nYPercent = 0;
       var rem = 0;
       
-      self.context.beginPath();
+      this.context.beginPath();
       // draw first point
       if (jsonPoints.length) {        
-        nYPercent = ((jsonPoints[0].tags.altitude - fLowAlt) / fRange) * 100;
-        nY = 1 + Math.round((self.nDrawHeight-2) - ((nYPercent * (self.nDrawHeight-2)) / 100));
-        self.context.moveTo(0, nY);
+        nYPercent = ((jsonPoints[0].tags.altitude - this.fLowAlt) / this.fAltRange) * 100;
+        nY = this.objTrailMarginRect.top + nYOffset + Math.round((self.nDrawHeight-2) - ((nYPercent * (self.nDrawHeight-2)) / 100));
+        self.context.moveTo(this.objTrailMarginRect.left, nY);
       }
       
       var nStartX = 0;  
-      fLowAlt = Math.round(fLowAlt);
       $.each(jsonPoints, function(key, point) {
         rem = key % Math.round(self.fXFactor * 4);
         if (rem == 0) {
-          nX = nXOffset + Math.round(key / self.fXFactor);
-          nYPercent = ((point.tags.altitude - fLowAlt) / fRange) * 100;
-          nY = 1 + Math.round((self.nDrawHeight-2) - ((nYPercent * (self.nDrawHeight-2)) / 100));
+          nX = nXOffset + self.objTrailMarginRect.left + Math.round(key / self.fXFactor);
+          nYPercent = ((point.tags.altitude - Math.round(self.fLowAlt)) / self.fAltRange) * 100;
+          nY = nYOffset + self.objTrailMarginRect.top + Math.round((self.nDrawHeight-2) - ((nYPercent * (self.nDrawHeight-2)) / 100));
           self.context.lineTo(nX, nY);            
           if (!nStartX) {
             nStartX = nX;
@@ -131,9 +177,46 @@ define([
       
       // draw last point
       if (jsonPoints.length) {     
-        self.context.lineTo(self.nCanvasWidth, nY);
+        this.context.lineTo(self.objTrailMarginRect.left + self.nDrawWidth, nY);
       }        
+      this.context.lineWidth = 4;
+      this.context.strokeStyle = 'rgba(68,182,252,1)';
+      this.context.stroke();      
+    },
+    addMediaMarker: function(nLat, nLng) {
+      var jsonPoints = this.model.get('value').route.route_points;
+      
+      var self = this;
+      var bFound = false;
+      
+      var nXOffset = (this.nCanvasDrawWidth - this.nDrawWidth) / 2;
+      var nYOffset = (this.nCanvasDrawHeight - this.nDrawHeight) / 2;
+      
+      $.each(jsonPoints, function(key, point) {
+        if (nLat == point.coords[1] && nLng == point.coords[0]) {
+          var nX = nXOffset + self.objTrailMarginRect.left + Math.round(key / self.fXFactor);
+          var nYPercent = ((point.tags.altitude - Math.round(self.fLowAlt)) / self.fAltRange) * 100;
+          var nY = nYOffset + self.objTrailMarginRect.top + Math.round((self.nDrawHeight-2) - ((nYPercent * (self.nDrawHeight-2)) / 100));
+          self.addMediaMarkerXY(nX, nY);
+          bFound = true;        
+        }
+      });
+      if (!bFound) {
+        this.addMediaMarkerXY(0, 0);
+      }
+    },
+    addMediaMarkerXY: function(nX, nY) {
+      nX -= 10;
+      nY -= 10;
+      
+      var elMarker = $('<div class="marker"></div>');
+      var elProfile = $('.profile', this.el);
+      elProfile.append(elMarker);
+
+      elMarker.css('left', nX);
+      elMarker.css('top', nY);      
     }
+    
   });
 
   return TrailAltitudeView;
