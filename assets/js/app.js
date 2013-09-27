a84f205362acd0e77a0b668387dfd86b10ddc0f3
Matt Allbeury <mallbeury@mac.com>
2013-09-27 15:29:33 +0200

    Completed Home page layout.

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,16 @@ define([
     });    
     handleResize();        
     
+    // register for image ready      
+    $('.tb-fade img', this.el).load(function() {
+      console.log('L:'+$(this).attr('class'));
+      $(this).parent().css({ opacity: 1 });
+    });
+    // force ie to run the load function if the image is cached
+    if ($('.tb-fade img', this.el).get(0).complete) {
+      $('.tb-fade img', this.el).trigger('load');
+    }
+        
     function handleResize() {
       $('.image').resizeToParent();      
     }
