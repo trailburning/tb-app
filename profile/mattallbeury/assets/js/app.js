93c48ed5479da0bbb6a813dfaca364c53967376f
Matt Allbeury <mallbeury@mac.com>
2013-10-30 15:06:29 +0100

    Test profile map click through.

--- profile/mattallbeury/assets/js/app.js
+++ profile/mattallbeury/assets/js/app.js
@@ -4,17 +4,19 @@ var RESTAPI_BASEURL = 'http://trailburning.herokuapp.com/api/';
 
 define([
   'underscore', 
-  'backbone'
-], function(_, Backbone){
+  'modernizr',
+  'backbone',
+  'views/ProfileMapView'
+], function(_, Modernizr, Backbone, ProfileMapView){
   app.dispatcher = _.clone(Backbone.Events);
   
   var initialize = function() {
-    this.userProfileMap = null;
+    this.profileMapView = new ProfileMapView({ el: '#profile_map_view' });
+    this.profileMapView.render();
         
     $(window).resize(function() {
       handleResize(); 
     });    
-    buildProfileMap();
     handleResize();
     
     $('#search_form').submit(function(evt) {
@@ -36,60 +38,6 @@ define([
     
     function handleResize() {
       $("img.scale_image_ready").imageScale();
-    }
-    
-    function buildProfileMap() {
-      var self = this;
-      
-      this.userProfileMap = L.mapbox.map('profile_map', 'mallbeury.test', {dragging: false, touchZoom: false, scrollWheelZoom:false, doubleClickZoom:false, boxZoom:false, tap:false, zoomControl:false, zoomAnimation:false, attributionControl:false});
-      this.userProfileMap.on('zoomend', function(e) {
-        if (self.userProfileMap.getZoom() > 5) {
-          self.userProfileMap.setZoom(5);
-        }
-      });
-
-      L.Control.Command = L.Control.extend({
-      options: {
-          position: 'topleft',
-      },
-      onAdd: function (map) {
-        var controlDiv = L.DomUtil.create('div', 'leaflet-overlay');
-        
-        return controlDiv;
-      }
-      });
-      
-      L.control.command = function (options) { return new L.Control.Command(options); };
-      
-      var overlay = L.control.command();
-      overlay.addTo(this.userProfileMap);
-      
-      var trailIcon = L.icon({
-          iconUrl: 'https://s3-eu-west-1.amazonaws.com/trailburning-assets/images/icons/marker_inactive.png',
-          iconSize:     [23, 24],
-          iconAnchor:   [10, 10]
-      });
-      
-      var LocationIcon = L.Icon.extend({
-          options: {
-              iconSize:     [36, 47],
-              iconAnchor:   [16, 44],
-              popupAnchor:  [16, 44]
-          }
-      });      
-      var locationIcon = new LocationIcon({iconUrl: 'https://s3-eu-west-1.amazonaws.com/trailburning-assets/images/icons/location.png'});
-      
-      var arrMarkers = [];
-          
-      arrMarkers.push([54.57269115373492, -2.9278992768377066]);                   
-      L.marker(arrMarkers[arrMarkers.length-1], {icon: trailIcon}).addTo(this.userProfileMap);
-
-      arrMarkers.push([52.5080060, 13.2574370]);                   
-      L.marker(arrMarkers[arrMarkers.length-1], {icon: locationIcon}).addTo(this.userProfileMap);
-
-      var bounds = new L.LatLngBounds(arrMarkers);
-      bounds = bounds.pad(0.05);
-      this.userProfileMap.fitBounds(bounds);       
     }    
   };
     
---
+++ profile/mattallbeury/assets/js/views/ProfileMapView.js
@@ -0,0 +1,60 @@
+define([
+  'underscore', 
+  'backbone'
+], function(_, Backbone){
+
+  var ProfileMapView = Backbone.View.extend({
+    initialize: function(){
+      this.userProfileMap = null;
+
+    },            
+    render: function(){
+      alert('pointerEvents:'+Modernizr.testAllProps('pointerEvents'));
+      
+      var self = this;
+      
+      this.userProfileMap = L.mapbox.map('profile_map', 'mallbeury.test', {dragging: true, touchZoom: false, scrollWheelZoom:false, doubleClickZoom:false, boxZoom:false, tap:false, zoomControl:false, zoomAnimation:false, attributionControl:false});      
+      this.userProfileMap.on('zoomend', function(e) {
+        if (self.userProfileMap.getZoom() > 5) {
+          self.userProfileMap.setZoom(5);
+        }
+      });
+
+      var trailIcon = L.icon({
+          iconUrl: 'https://s3-eu-west-1.amazonaws.com/trailburning-assets/images/icons/marker_inactive.png',
+          iconSize:     [23, 24],
+          iconAnchor:   [10, 10]
+      });
+      
+      var LocationIcon = L.Icon.extend({
+          options: {
+              iconSize:     [36, 47],
+              iconAnchor:   [16, 44],
+              popupAnchor:  [16, 44]
+          }
+      });      
+      var locationIcon = new LocationIcon({iconUrl: 'https://s3-eu-west-1.amazonaws.com/trailburning-assets/images/icons/location.png'});
+      
+      var arrMarkers = [];
+
+      function onClick(e) {
+        
+//      alert(Modernizr.testAllProps('pointerEvents'));
+        console.log(Modernizr.touch);
+        
+      }
+          
+      arrMarkers.push([54.57269115373492, -2.9278992768377066]);                   
+      L.marker(arrMarkers[arrMarkers.length-1], {icon: trailIcon, zIndexOffset: 1000}).on('click', onClick).addTo(this.userProfileMap);
+
+      arrMarkers.push([52.5080060, 13.2574370]);                   
+      L.marker(arrMarkers[arrMarkers.length-1], {icon: locationIcon, zIndexOffset: 1000}).on('click', onClick).addTo(this.userProfileMap);
+
+      var bounds = new L.LatLngBounds(arrMarkers);
+      bounds = bounds.pad(0.05);
+      this.userProfileMap.fitBounds(bounds);
+    }    
+  });
+
+  return ProfileMapView;
+});
