01b53c56f084eaf027fe826071d330aef23ee7ce
Matt Allbeury <mallbeury@mac.com>
2014-01-10 12:54:40 +0100

    Updated editorial page.

--- web/demos/editorial/jan/assets/js/views/TrailMapMediaMarkerView.js
+++ web/demos/editorial/jan/assets/js/views/TrailMapMediaMarkerView.js
@@ -8,11 +8,13 @@ define([
 ], function(_, Backbone){
 
   var TrailMapMediaMarkerView = Backbone.View.extend({
-    
     initialize: function(){
       this.map = this.options.map;
       this.marker = null;
       this.nSize = DEF_ICONS;
+      this.bEnablePopup = false;
+      this.popup = null;
+      this.bActive = false;
       if (this.options.size) {
         this.nSize = this.options.size;
       }
@@ -24,7 +26,7 @@ define([
             options: {
                 iconSize:     [23, 24],
                 iconAnchor:   [11, 11],
-                popupAnchor:  [11, 11]
+                popupAnchor:  [0, 0]
             }
           });              
           this.mediaInactiveIcon = new MediaIcon({iconUrl: ASSETS_BASEURL + 'images/icons/marker_inactive.png'});
@@ -36,7 +38,7 @@ define([
             options: {
                 iconSize:     [18, 18],
                 iconAnchor:   [8, 8],
-                popupAnchor:  [8, 8]
+                popupAnchor:  [0, 0]
             }
           });      
           this.mediaInactiveIcon = new MediaIcon({iconUrl: ASSETS_BASEURL + 'images/icons/sm_marker_inactive.png'});
@@ -50,14 +52,48 @@ define([
     hide: function(){
       this.marker.setOpacity(0);
     },    
+    enablePopup: function(bEnable){
+      this.bEnablePopup = bEnable;
+      if (this.bEnablePopup && this.bActive) {
+        this.showPopup();      
+      }
+      else {
+        this.hidePopup();
+      }
+    },
+    showPopup: function(){
+      this.popup.openOn(this.map);
+      var imgLoad = imagesLoaded('.trail_media_popup .scale');
+      imgLoad.on('always', function(instance) {
+        for ( var i = 0, len = imgLoad.images.length; i < len; i++ ) {
+          $(imgLoad.images[i].img).addClass('scale_image_ready');
+        }
+        // update pos
+        $('.trail_media_popup img.scale_image_ready').imageScale();
+        // fade in - delay adding class to ensure image is ready  
+        $('.trail_media_popup .fade_on_load').addClass('tb-fade-in');
+        $('.trail_media_popup .image_container').css('opacity', 1);
+      });
+    },
+    hidePopup: function(){
+      this.map.closePopup();
+      $('.trail_media_popup .image_container').css('opacity', 0);
+    },
     setActive: function(bActive){
+      this.bActive = bActive;
       if (bActive) {
         this.marker.setIcon(this.mediaActiveIcon);
-        this.marker.setZIndexOffset(200);
+        this.marker.setZIndexOffset(200);        
+        if (this.bEnablePopup) {        
+          this.showPopup();
+        }
       }
       else {
         this.marker.setIcon(this.mediaInactiveIcon);
         this.marker.setZIndexOffset(100);
+        if (this.bEnablePopup) {        
+          this.hidePopup();          
+        }
       }
     },    
     render: function(){
@@ -68,6 +104,13 @@ define([
         // fire event
         app.dispatcher.trigger("TrailMapMediaMarkerView:mediaclick", self);                        
       }
+
+      // build popup      
+      var versions = this.model.get('versions');
+      this.popup = L.popup({'closeButton': false})
+      .setLatLng([this.model.get('coords').lat, this.model.get('coords').long])
+      .setContent('<div class="trail_media_popup"><div class="image_container fade_on_load tb-fade"><img src="http://app.resrc.it/o=80/http://s3-eu-west-1.amazonaws.com/'+versions[0].path+'" class="resrc scale"></div></div>');
+      
       return this;
     }    
   });
