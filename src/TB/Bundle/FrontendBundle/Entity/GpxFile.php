c38dda2df2a9c1a121e86251efdeeace78bafa9c
ptrost <--replace-all>
2014-03-05 13:36:08 +0100

    Added Gaufrette to handle S3 storage

--- src/TB/Bundle/FrontendBundle/Entity/GpxFile.php
+++ src/TB/Bundle/FrontendBundle/Entity/GpxFile.php
@@ -3,6 +3,8 @@
 namespace TB\Bundle\FrontendBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * GpxFile
@@ -28,7 +30,10 @@ class GpxFile
      */
     private $id;
 
-
+    /**
+     * @Assert\File(maxSize="6000000")
+     */
+    private $gpxfile;
 
     /**
      * Set path
@@ -61,5 +66,49 @@ class GpxFile
     public function getId()
     {
         return $this->id;
+    }
+    
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setGpxfile(UploadedFile $gpxfile = null)
+    {
+       $this->gpxfile = $gpxfile;
+    }
+
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getGpxfile()
+    {
+       return $this->gpxfile;
+    }
+    
+    public function upload()
+    {
+        // the file property can be empty if the field is not required
+        if (null === $this->getGpxfile()) {
+            return;
+        }
+
+        // use the original file name here but you should
+        // sanitize it at least to avoid any security issues
+
+        // move takes the target directory and then the
+        // target filename to move to
+        $this->getGpxfile()->move(
+            $this->getUploadRootDir(),
+            $this->getGpxfile()->getClientOriginalName()
+        );
+
+        // set the path property to the filename where you've saved the file
+        $this->path = $this->getGpxfile()->getClientOriginalName();
+
+        // clean up the file property as you won't need it anymore
+        $this->file = null;
     }
 }
