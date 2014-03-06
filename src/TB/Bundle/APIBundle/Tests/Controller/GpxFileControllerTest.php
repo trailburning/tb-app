535bc2d1e3fd0e94d451b58c8194317326cf2f14
ptrost <--replace-all>
2014-03-06 14:30:10 +0100

    fixed merge conflict

--- src/TB/Bundle/APIBundle/Tests/Controller/GpxFileControllerTest.php
+++ src/TB/Bundle/APIBundle/Tests/Controller/GpxFileControllerTest.php
@@ -23,14 +23,27 @@ class GpxFileControllerTest extends AbstractApiTestCase
      */
     public function testPostImport()
     {
+        $this->loadFixtures([
+            'TB\Bundle\FrontendBundle\DataFixtures\ORM\RouteData',
+        ]);
+        
+        // Get User from DB with the slug "mattallbeury"..
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $user = $em
+            ->getRepository('TBFrontendBundle:User')
+            ->findOneByName('mattallbeury');
+        
+        if (!$user) {
+            $this->fail('Missing User with name "mattallbeury" in test DB');
+        }
+        
         $gpxfile = new UploadedFile(
             realpath(__DIR__ . '/../../DataFixtures/GpxFiles/example.gpx'),
             'example.gpx'
         );
         
         $client = $this->createClient();
-
-        $crawler = $client->request('POST', '/v1/import/gpx', array(), array('gpxfile' => $gpxfile));
+        $crawler = $client->request('POST', '/v1/import/gpx', [], ['gpxfile' => $gpxfile], ['HTTP_Trailburning_User_ID' => $user->getId()]);
         
         echo $client->getResponse()->getContent();
         exit;
