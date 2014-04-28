adc97932958fbc514074efad103f0fd81470b126
ptrost <--replace-all>
2014-04-28 10:27:21 +0200

    Added 404 error page

--- app/frontend/AppKernel.php
+++ app/frontend/AppKernel.php
@@ -42,6 +42,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+            $bundles[] = new Webfactory\Bundle\ExceptionsBundle\WebfactoryExceptionsBundle();
         }
         
         if (in_array($this->getEnvironment(), array('test'))) {
