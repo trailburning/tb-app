17aa6b66d17cab47cda6e434b69703d89e569b32
ptrost <--replace-all>
2014-05-10 10:22:41 +0200

    Added memcached session config

---
+++ app/config/session.php
@@ -0,0 +1,40 @@
+<?php 
+
+use Symfony\Component\DependencyInjection\Definition;
+
+if (class_exists('\\Memcached')) {
+    // Use Memcached session handler when tho memcached module is installed
+    $memcached = $container->setDefinition('session.memcached', new Definition(
+        'Memcached',
+        ['%session_prefix%']
+    ))->addMethodCall('addServer', [
+        '%memcached_host%',
+        '%memcached_port%',
+    ]);
+
+    // Only set SASL authenfification when parameters are set
+    if ($container->getParameter('memcached_username') != '') {
+        $memcached->addMethodCall('setSaslAuthData', [
+            '%memcached_username%',
+            '%memcached_password%',
+        ]);
+    }
+
+    $container->setDefinition('session.handler.memcached', new Definition(
+        'Symfony\Component\HttpFoundation\Session\Storage\Handler\MemcachedSessionHandler',
+        [$memcached, [ 'prefix' => '%session_prefix%', 'expiretime' => '%session_expire%' ]]
+    ));
+    
+    $container->loadFromExtension('framework', [
+        'session' => [
+            'handler_id' => 'session.handler.memcached'
+        ],
+    ]);
+} else {
+    // Otherwise use the default session handler
+    $container->loadFromExtension('framework', [
+        'session' => null
+    ]);
+}
+
+
