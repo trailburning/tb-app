df752613e6057ae2d20d6038401f0ca110fad973
Matt Allbeury <mallbeury@mac.com>
2014-05-19 10:06:03 +0200

    Fix case.

--- web/demos/photo/assets/js/main.js
+++ web/demos/photo/assets/js/main.js
@@ -8,7 +8,7 @@ require.config({
 	}
 });
 // Load our app module and pass it to our definition function
-require(['controller/app'], function(App){
+require(['controller/App'], function(App){
 	App.initialize();
 })
 define('gmaps', ['async!https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places'], function() {
