77380b3ddaf559489d510e4d5c63cd35a55b181c
ptrost <--replace-all>
2014-05-22 10:45:46 +0200

    deleted old api files

--- api/templates/ImportGpx.php
+++
@@ -1,4 +0,0 @@
-<form action="/trailburning_api/v1/import/gpx" method="post" enctype="multipart/form-data">
-    <input type="file" name="gpxfile">
-    <input type="submit">
-</form>
--- api/tests/tb-restapi/DeleteNonExistingRouteCept.php
+++
@@ -1,8 +0,0 @@
-<?php
-use \ApiGuy;
-
-$I = new ApiGuy($scenario);
-$I->wantTo('Delete a route that doesn\'t exist');
-$I->SendDELETE('/v1/route/999999');
-$I->seeResponseCodeIs(404);
-$I->seeResponseIsJson();
