27b751a81794126496f5912ec72aa9f82365a988
ptrost <--replace-all>
2014-05-22 14:45:28 +0200

    Added custom form type for the profile form

---
+++ src/TB/Bundle/FrontendBundle/Form/Type/ProfileFormType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace TB\Bundle\FrontendBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
+use TB\Bundle\FrontendBundle\Entity\User;
+
+/**
+ * Extend the registration form from of the FOSUserBundle to add custom fields and functionallity
+ */
+class ProfileFormType extends BaseType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        // add your custom fields not defined in FOSUserBundle
+        $builder->add('firstName');
+        $builder->add('lastName');
+        $builder->add('location', 'text', ['label' => 'Where do you call home?']);
+        $builder->add('about', 'textarea', ['label' => 'Tell us a little bit about yourself']);
+        $builder->add('gender', 'choice', [
+            'label' => 'What gender are you?',
+            'choices' => [
+                User::GENDER_NONE => 'I\'d rather not say',
+                User::GENDER_MALE => 'Male',
+                User::GENDER_FEMALE => 'Female',
+            ],
+        ]);
+        
+        $builder->add('newsletter', 'checkbox', ['label' => 'Receive Trailburning newsletter', 'required' => false]);
+        
+        $builder->remove('username');
+    }
+
+    public function getName()
+    {
+        return 'tb_user_profile';
+    }
+}
