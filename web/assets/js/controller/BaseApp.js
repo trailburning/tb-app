8d530de36fdd8deb3dfb8c2f5691a4f4a2450579
Matt Allbeury <mallbeury@mac.com>
2014-08-20 11:46:00 +0200

    Added hero slides to ambassador page.

--- web/assets/js/controller/BaseApp.js
+++ web/assets/js/controller/BaseApp.js
@@ -3,8 +3,9 @@ var app = app || {};
 define([
   'underscore', 
   'backbone',
-  'views/ActivityFeedView'  
-], function(_, Backbone, ActivityFeedView){
+  'views/ActivityFeedView',
+  'views/HerosView'  
+], function(_, Backbone, ActivityFeedView, HerosView){
   app.dispatcher = _.clone(Backbone.Events);
   
   var initialize = function() {
@@ -26,27 +27,38 @@ define([
     if ($('.cookie_error').length) {
       checkCookies();	
     }    
-    
-    $('#search_field').focus(function(evt) {
-      $('#search_field').val('not just yet...');
-      event.preventDefault();
-    });
-    $('#search_form').submit(function(evt) {
-      $('#search_field').val('not just yet...');
-      event.preventDefault();
-    });    
-    
-    var imgLoad = imagesLoaded('.scale');
-    imgLoad.on('always', function(instance) {
-      for ( var i = 0, len = imgLoad.images.length; i < len; i++ ) {
-        $(imgLoad.images[i].img).addClass('scale_image_ready');
-      }
-      // update pos
-      $("img.scale_image_ready").imageScale();
-      // fade in - delay adding class to ensure image is ready  
-      $('.fade_on_load').addClass('tb-fade-in');
-      $('.image_container').css('opacity', 1);
-    });
+
+	this.herosView = null;
+	if ($('#hero_images').length) {		
+      this.herosView = new HerosView({ el: '#hero_images' });
+	  this.herosView.render();
+	  
+      var imgLoad = imagesLoaded('.panel_content .scale, promo_content .scale');
+      imgLoad.on('always', function(instance) {
+        for ( var i = 0, len = imgLoad.images.length; i < len; i++ ) {
+          $(imgLoad.images[i].img).addClass('scale_image_ready');
+        }
+        // update pos
+        $(".panel_content img.scale_image_ready, .promo_content img.scale_image_ready").imageScale();
+        // fade in - delay adding class to ensure image is ready  
+        $('.panel_content .fade_on_load, .promo_content .fade_on_load').addClass('tb-fade-in');
+        $('.panel_content .image_container, .promo_content .image_container').css('opacity', 1);
+      });	  
+	}
+	else {
+      var imgLoad = imagesLoaded('.scale');
+      imgLoad.on('always', function(instance) {
+        for ( var i = 0, len = imgLoad.images.length; i < len; i++ ) {
+          $(imgLoad.images[i].img).addClass('scale_image_ready');
+        }
+        // update pos
+        $("img.scale_image_ready").imageScale();
+        // fade in - delay adding class to ensure image is ready  
+        $('.fade_on_load').addClass('tb-fade-in');
+        $('.image_container').css('opacity', 1);
+      });
+		
+	}
 
     function checkCookies() {    
 	  $.cookie('test', 'trailburning');
