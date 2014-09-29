e22615f9c1ae4fd31b28c4749e66c38890d5db6f
Matt Allbeury <mallbeury@mac.com>
2014-09-29 14:44:43 +0200

    Added altitude formatting.

--- web/assets/js/views/TrailStatsView.js
+++ web/assets/js/views/TrailStatsView.js
@@ -18,18 +18,6 @@ define([
         return;
       }
       
-      function formatAltitude(nStr){
-        nStr += '';
-        x = nStr.split('.');
-        x1 = x[0];
-        x2 = x.length > 1 ? '.' + x[1] : '';
-        var rgx = /(\d+)(\d{3})/;
-        while (rgx.test(x1)) {
-          x1 = x1.replace(rgx, '$1' + '’' + '$2');
-        }
-        return x1 + x2;
-      }
-      
       if (!this.model) {
         return;
       }
