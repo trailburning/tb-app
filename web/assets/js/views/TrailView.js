1c772649aa47973dba5632be406e70e92119ef19
Matt Allbeury <mallbeury@mac.com>
2014-09-30 13:45:07 +0200

    Added map and demo 'like panel'.

--- web/assets/js/views/TrailView.js
+++ web/assets/js/views/TrailView.js
@@ -288,6 +288,22 @@ define([
       $(window).mousemove(function(evt) {
         self.tickle();
       });
+      
+      var jsonPoint = this.model.get('value').route.route_points[0]; 
+      var map = L.mapbox.map('trail_location_map', 'mallbeury.map-kply0zpa', {dragging: false, touchZoom: false, scrollWheelZoom:false, doubleClickZoom:false, boxZoom:false, tap:false, zoomControl:false, zoomAnimation:false, attributionControl:false});
+      var LocationIcon = L.Icon.extend({
+          options: {
+              iconSize:     [36, 47],
+              iconAnchor:   [16, 44],
+              popupAnchor:  [16, 44]
+          }
+      });      
+      var startIcon = new LocationIcon({iconUrl: 'http://assets.trailburning.com/images/icons/location.png'});
+      L.marker([jsonPoint.coords[1], jsonPoint.coords[0]], {icon: startIcon}).addTo(map);      
+
+      var latlng = new L.LatLng(jsonPoint.coords[1], jsonPoint.coords[0]);
+      map.setView(latlng, 12);
+      
     },
     handleMedia: function(){
       var self = this;
