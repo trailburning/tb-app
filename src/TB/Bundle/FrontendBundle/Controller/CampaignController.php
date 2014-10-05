df97bad7f784a66e6ad724c8365c85dfe0f3f2a4
ptrost <info@cynova.net>
2014-10-05 19:21:57 +0200

    initial campaign page

---
+++ src/TB/Bundle/FrontendBundle/Controller/CampaignController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace TB\Bundle\FrontendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+class CampaignController extends Controller
+{
+    
+    
+    /**
+     * @Route("/campaign/{slug}", name="campaigns")
+     * @Template()
+     */
+    public function campaignAction($slug)
+    {
+        $breadcrumb = array();
+        
+        $campaign = $this->getDoctrine()
+            ->getRepository('TBFrontendBundle:Campaign')
+            ->findOneBySlug($slug);
+
+        if (!$campaign) {
+            throw $this->createNotFoundException(
+                sprintf('Campaign %s not found', $slug)
+            );
+        }
+        
+        $breadcrumb[] = [
+            'name' => 'campaign',
+            'label' => trim($campaign->getTitle()), 
+            'params' => ['slug' => $campaign->getSlug()],
+        ];
+        
+        return array(
+            'campaign' => $campaign,
+            'breadcrumb' => $breadcrumb,
+        );
+    }
+    
+}
