b620efc6e7eef3400e06e26b6bb74200c2a2cddc
Matt Allbeury <mallbeury@mac.com>
2014-10-09 18:40:23 +0200

    Added search to all views.

--- web/assets/js/controller/HomeApp.js
+++ web/assets/js/controller/HomeApp.js
@@ -4,8 +4,9 @@ define([
   'underscore', 
   'backbone',
   'views/ActivityFeedView',
+  'views/SearchView',
   'views/HomeHerosView'      
-], function(_, Backbone, ActivityFeedView, HomeHerosView){
+], function(_, Backbone, ActivityFeedView, SearchView, HomeHerosView){
   app.dispatcher = _.clone(Backbone.Events);
   
   var initialize = function() {
@@ -44,6 +45,7 @@ define([
 	  resrc.resrcAll();
 	});        
         
+	this.searchView = new SearchView({ el: '#searchview' });        
     if (typeof TB_USER_ID != 'undefined') {
   	  this.activityFeedView = new ActivityFeedView({ el: '#activity_feed_view' });
   	  this.activityFeedView.render();
@@ -53,78 +55,6 @@ define([
     this.homeHerosView = new HomeHerosView({ el: '#home_header' });
 	this.homeHerosView.render();
 	
-	var cache = {};
-
-	$('#search').submit(function(evt) {
-  	  evt.preventDefault();
-	});
-	
-	// setup autosuggest
-    $('#searchBox').autocomplete({
-        minLength: 2,
-        delay: 0,
-        select: function(event, ui)  {
-        	window.location = ui.item.url;
-        },
-        source: function(request, response ) {
-            var term = request.term;
-            if (term in cache) {
-                response(cache[term]);
-                return;
-            }
-            var url = TB_RESTAPI_BASEURL + '/v1/search/suggest?q=' + term;
-            $.getJSON(url, request, function( data, status, xhr ) {
-                var suggestions = data.hits.hits;
-                cache[term] = suggestions;
-                response(suggestions);
-            });
-        }    
-	});
-	
-    $('#searchBox').data('ui-autocomplete')._resizeMenu = function() {
-    	this.menu.element.outerWidth(300);
-    };
-    $('#searchBox').data('ui-autocomplete')._renderItem = function(ul, item) {
-    	var strItem = "", strURL = '';
-    	var text = item._source.suggest_engram;
-    	
-        if (item.highlight) {
-            if (item.highlight.suggest_engram_full) {
-                text = item.highlight.suggest_engram_full;
-            } else if(item.highlight.suggest_engram_part) {
-                text = item.highlight.suggest_engram_part;
-            } else {
-                text = item.highlight.suggest_engram;
-            }
-        }
-    	
-    	switch (item._type) {
-    	  case 'user_profile':
-    	    strURL = 'profile/' + item._source.name;
-    	    strItem = '<a href="' + strURL + '" class="clearfix"><div class="type"><div class="tb-avatar tb-avatar-search"><div class="photo"><img src="'+item._source.avatar+'"></div></div></div><div class="match">' + text + '<br/>Discover ' + item._source.first_name + '\'s trails.</div></a>';
-    	    break;
-    	  case 'event':
-    	    strURL = 'event/' + item._source.slug
-    	    strItem = '<a href="' + strURL + '" class="clearfix"><div class="type"><div class="icon_container"><div class="icon event"></div></div></div><div class="match">' + text + '</div></a>';
-    	    break;
-    	  case 'editorial':
-    	    strURL = 'editorial/' + item._source.slug
-    	    strItem = '<a href="' + strURL + '" class="clearfix"><div class="type"><div class="icon_container"><div class="icon editorial"></div></div></div><div class="match">' + text + '</div></a>';
-    	    break;
-    	  default:
-    	    strURL = 'trail/' + item._source.slug
-    	    strItem = '<a href="' + strURL + '" class="clearfix"><div class="type"><div class="icon_container"><div class="icon trailcard"></div></div></div><div class="match">' + text + '</div></a>';
-    	    break;
-    	}
-
-    	item.url = strURL;
-		console.log(item);    	
-
-        return $('<li>')
-            .append(strItem)
-            .appendTo(ul);
-    };
-    
   	// keyboard control
   	$(document).keydown(function(e){
   	  switch (e.keyCode) {
