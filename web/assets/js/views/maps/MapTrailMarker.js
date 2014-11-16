38f9c2f45c0557fb7665c4261c93256243ac6284
Matt Allbeury <mallbeury@mac.com>
2014-11-16 16:26:56 +0100

    Moved popup to template.

--- web/assets/js/views/maps/MapTrailMarker.js
+++ web/assets/js/views/maps/MapTrailMarker.js
@@ -1,8 +1,9 @@
 define([
   'underscore', 
   'backbone',
-  'models/TrailModel'
-], function(_, Backbone, TrailModel){
+  'models/TrailModel',
+  'views/maps/MapTrailMarkerPopup'
+], function(_, Backbone, TrailModel, MapTrailMarkerPopup){
 
   var MapTrailMarker = Backbone.View.extend({
     initialize: function(){
@@ -11,7 +12,6 @@ define([
   	  this.trailEvents.dispatcher = _.clone(Backbone.Events);
     	
 	  var self = this;
-	  this.popup = null;
 	      	
       this.trailEvents.dispatcher.on("DistanceMarkers:click", function(evt){
       	self.showPopup();
@@ -49,103 +49,10 @@ define([
       this.bTrailVisible = false;   
     },
     showPopup: function(){
-      var popup_options = {
-        autoPan: true,
-        closeButton: true,
-        maxWidth: 460,
-        autoPanPadding: [30, 30],
-        offset: [0, -15]
-      };                
-        
-      this.popup = L.popup(popup_options)
-      .setLatLng([this.marker.getLatLng().lat, this.marker.getLatLng().lng])
-      .setContent(this.popupContainer[0])
-      .openOn(this.options.map);  
-      
-  	  // reset      
-      $('.tb-trailpopup.fade_on_load').removeClass('tb-fade-in');
-      $('.tb-trailpopup.image_container').css('opacity', 0);
-            
-      var nRating = this.model.get('rating');
-      if (!nRating) {
-        // do not show no stars
-       	$('.tb-trailpopup .stars').hide();
-      }
-      
-      $.each($('.tb-trailpopup .star'), function(index, value){
-		switch (index) {
-          case 0:
-            if (nRating > 0) {
-          	  $(this).addClass('star_full');          	  	
-          	}
-          	break;
-          case 1:
-          	if (nRating >= 2) {
-          	  $(this).addClass('star_full');          	  	          	  
-          	}
-          	else if (nRating >= 1.5) {
-          	  $(this).addClass('star_half');          	  	          	  
-          	}
-          	else {
-          	  $(this).addClass('star');          	  	          	  
-          	}
-          	break;
-          case 2:
-          	if (nRating >= 3) {
-          	  $(this).addClass('star_full');          	  	          	  
-          	}
-          	else if (nRating >= 2.5) {
-          	  $(this).addClass('star_half');          	  	          	  
-          	}
-          	else {
-          	  $(this).addClass('star');          	  	          	  
-          	}
-          	break;
-          case 3:
-          	if (nRating >= 4) {
-          	  $(this).addClass('star_full');          	  	          	  
-          	}
-          	else if (nRating >= 3.5) {
-          	  $(this).addClass('star_half');          	  	          	  
-          	}
-          	else {
-          	  $(this).addClass('star');          	  	          	  
-          	}
-          	break;
-          case 4:
-          	if (nRating >= 5) {
-          	  $(this).addClass('star_full');          	  	          	  
-          	}
-          	else if (nRating >= 4.5) {
-          	  $(this).addClass('star_half');          	  	          	  
-          	}
-          	else {
-          	  $(this).addClass('star');          	  	          	  
-          	}
-          	break;
-          }        	
-      });      
-      
-	  // scale images when loaded
-	  var elImages = $('.tb-trailpopup .scale');
-	  var imgLoad = imagesLoaded(elImages);
-      imgLoad.on('always', function(instance) {
-        for ( var i = 0, len = imgLoad.images.length; i < len; i++ ) {
-          $(imgLoad.images[i].img).addClass('scale_image_ready');
-        }
-        // update pos
-        $('.tb-trailpopup img.scale_image_ready').imageScale();
-        // fade in - delay adding class to ensure image is ready  
-        $('.tb-trailpopup .fade_on_load').addClass('tb-fade-in');
-        $('.tb-trailpopup .image_container').css('opacity', 1);
-      });
-	  // invoke resrc      
-      resrc.resrc($('.tb-trailpopup .scale'));                      
+      this.popup.show();
     },    
     hidePopup: function(){
-      if (this.popup) {
-     	this.options.map.closePopup(this.popup);
-      }
+      this.popup.hide();
     },
     render: function(){
       var self = this;
@@ -168,78 +75,8 @@ define([
 	    this.marker.setIcon(L.divIcon({className: 'tb-map-location-marker', html: '<div class="marker"></div>', iconSize: [18, 25], iconAnchor: [9, 25]}));      	  
 		this.options.mapCluster.addLayer(this.marker);
 
-        this.popupContainer = $('<div />');      
-        this.popupContainer.on('click', '.btnView', function(evt) {
-          // fire event          
-          app.dispatcher.trigger("MapTrailDetail:click", this);                        
-        });
-        
-	    if (this.model.get('category') == undefined) {
-		  this.model.set('category', '');
-        }
-
-	    if (this.model.get('length')) {
-	      this.model.set('length_km', Math.ceil(this.model.get('length') / 1000));
-	      this.model.set('ascent_m', formatAltitude(Math.round(this.model.get('tags').ascent)));
-	      this.model.set('descent_m', formatAltitude(Math.round(this.model.get('tags').descent)));	  	
-	    }
-
-	  	var bEvent = false;
-      	switch (this.model.get('slug')) {
-          case '16km':
-          case '30km':
-          case '46km':
-            bEvent = true;	          	
-            this.model.set('sponsorURL', 'event/ultraks');
-            break;	          	  
-          case 'e16':
-          case 'e51':
-          case 'e101':
-            bEvent = true;	          	
-            this.model.set('sponsorURL', 'event/eiger');
-            break;	          	  
-          case 'ttm':
-            bEvent = true;	          	
-            this.model.set('sponsorURL', 'event/tfor');
-            break;	          	  
-          case 'marathon':
-            bEvent = true;	          	
-            this.model.set('sponsorURL', 'event/aom');
-            break;	          	  
-          case 'ultramarathon':
-            bEvent = true;	          	
-            this.model.set('sponsorURL', 'event/laugavegur');
-            break;	          	  
-      	  case 'lantau-vertical-hong-kong':
-      	    bEvent = true;	          	
-      	    this.model.set('sponsorURL', 'event/lantauvertical');
-      	    break;	          	  	          	  
-      	  case 'heysen-105-south-australia':
-      	    bEvent = true;	          	
-      	    this.model.set('sponsorURL', 'event/heysen105');
-      	    break;	          
-      	  case 'scenic-trail-24k-d-2200m-lugano-capriasca-switzerland':
-      	  case 'scenic-trail-54k-d-3900m-lugano-capriasca-switzerland':
-      	    bEvent = true;	          	
-      	    this.model.set('sponsorURL', 'event/luganoscenictrail');
-        }
-
-        if (this.model.get('user').type == 'brand') {
-	      this.model.set('sponsorURL', 'profile/' + this.model.get('user').name);
-  	      bEvent = true;
-        }
-
-		var strCategory = '<h3 class="tb">'+this.model.get('category').name+'</h3>';
-		// mla - this should be an id
-		if (this.model.get('category').name == 'Mountains') {
-		  strCategory = '<h3 class="tb reduce">'+this.model.get('category').name+'</h3>';
-		}
-
-	    var strPopup = '<div class="tb-trailpopup"><div class="card"><div class="image_container fade_on_load"><img data-src="http://app.resrc.it/O=80/http://media.trailburning.com'+this.model.get('media').versions[0].path+'" class="resrc scale"></div><div class="card_title"><h1>'+this.model.get('name')+'</h1><br/><h2>'+this.model.get('region')+'</h2><br/><h2><span class="stars"><span class="star"></span><span class="star"></span><span class="star"></span><span class="star"></span><span class="star"></span></span></h2></div></div><div class="card_avatar"><div class="tb-avatar"><div class="photo"><a href="'+TB_BASEURL+'/profile/'+this.model.get('user').name+'"><img src="'+this.model.get('user').avatar+'"></a></div></div></div><div class="detail_container">'+strCategory+'<div class="summary"><div class="length">'+this.model.get('length_km')+' km</div><div class="altitude">'+this.model.get('ascent_m')+' D+<br/>'+this.model.get('descent_m')+' D-</div></div><div class="btns"><span data-url="'+TB_PATH+'/trail/'+this.model.get('slug')+'" data-id="'+this.model.id+'" class="btn btn-tb-action btnView">View the Trail</span></div></div></div>';              
-	    if (bEvent) {
-	      strPopup = '<div class="tb-trailpopup"><div class="card"><div class="image_container fade_on_load"><img data-src="http://app.resrc.it/O=80/http://media.trailburning.com'+this.model.get('media').versions[0].path+'" class="resrc scale"></div><div class="card_title"><h1>'+this.model.get('name')+'</h1><br/><h2>'+this.model.get('region')+'</h2></div></div><div class="card_avatar"><a href="'+TB_BASEURL+'/'+this.model.get('sponsorURL')+'"><img src="http://assets.trailburning.com/images/'+this.model.get('sponsorURL')+'/card_trail_sticker_logo.png"></a></div><div class="detail_container">'+strCategory+'<div class="summary"><div class="length">'+this.model.get('length_km')+' km</div><div class="altitude">'+this.model.get('ascent_m')+' D+<br/>'+this.model.get('descent_m')+' D-</div></div><div class="btns"><span data-url="'+TB_PATH+'/trail/'+this.model.get('slug')+'" data-id="'+this.model.id+'" class="btn btn-tb-action btnView">View the Trail</span></div></div></div>';              
-	    }
-      	this.popupContainer.html(strPopup);      	
+	    this.popup = new MapTrailMarkerPopup({ model: this.model, map: this.options.map });
+		this.popup.render();
 	  }
       this.bRendered = true;
                        
---
+++ web/assets/js/views/maps/MapTrailMarkerPopup.js
@@ -0,0 +1,209 @@
+define([
+  'underscore', 
+  'backbone'
+], function(_, Backbone){
+
+  var MapTrailMarkerPopup = Backbone.View.extend({
+    initialize: function(){
+	  this.popup = null;
+    },            
+    show: function(fLat, fLng){    
+      var popup_options = {
+        autoPan: true,
+        closeButton: true,
+        maxWidth: 460,
+        autoPanPadding: [30, 30],
+        offset: [0, -15]
+      };                
+        
+      this.popup = L.popup(popup_options)
+      .setLatLng([this.model.get('start')[1], this.model.get('start')[0]])
+      .setContent(this.popupContainer[0])
+      .openOn(this.options.map);  
+      
+  	  // reset      
+      $('.tb-trailpopup.fade_on_load').removeClass('tb-fade-in');
+      $('.tb-trailpopup.image_container').css('opacity', 0);
+            
+      var nRating = this.model.get('rating');
+      if (!nRating) {
+        // do not show no stars
+       	$('.tb-trailpopup .stars').hide();
+      }
+      
+      // mla - this should be an id
+	  if (this.model.get('category').name == 'Mountains') {
+	  	$('.tb-trailpopup h3').addClass('reduce');
+	  }
+      
+      $.each($('.tb-trailpopup .star'), function(index, value){
+		switch (index) {
+          case 0:
+            if (nRating > 0) {
+          	  $(this).addClass('star_full');          	  	
+          	}
+          	break;
+          case 1:
+          	if (nRating >= 2) {
+          	  $(this).addClass('star_full');          	  	          	  
+          	}
+          	else if (nRating >= 1.5) {
+          	  $(this).addClass('star_half');          	  	          	  
+          	}
+          	else {
+          	  $(this).addClass('star');          	  	          	  
+          	}
+          	break;
+          case 2:
+          	if (nRating >= 3) {
+          	  $(this).addClass('star_full');          	  	          	  
+          	}
+          	else if (nRating >= 2.5) {
+          	  $(this).addClass('star_half');          	  	          	  
+          	}
+          	else {
+          	  $(this).addClass('star');          	  	          	  
+          	}
+          	break;
+          case 3:
+          	if (nRating >= 4) {
+          	  $(this).addClass('star_full');          	  	          	  
+          	}
+          	else if (nRating >= 3.5) {
+          	  $(this).addClass('star_half');          	  	          	  
+          	}
+          	else {
+          	  $(this).addClass('star');          	  	          	  
+          	}
+          	break;
+          case 4:
+          	if (nRating >= 5) {
+          	  $(this).addClass('star_full');          	  	          	  
+          	}
+          	else if (nRating >= 4.5) {
+          	  $(this).addClass('star_half');          	  	          	  
+          	}
+          	else {
+          	  $(this).addClass('star');          	  	          	  
+          	}
+          	break;
+          }        	
+      });      
+      
+	  // scale images when loaded
+	  var elImages = $('.tb-trailpopup .scale');
+	  var imgLoad = imagesLoaded(elImages);
+      imgLoad.on('always', function(instance) {
+        for ( var i = 0, len = imgLoad.images.length; i < len; i++ ) {
+          $(imgLoad.images[i].img).addClass('scale_image_ready');
+        }
+        // update pos
+        $('.tb-trailpopup img.scale_image_ready').imageScale();
+        // fade in - delay adding class to ensure image is ready  
+        $('.tb-trailpopup .fade_on_load').addClass('tb-fade-in');
+        $('.tb-trailpopup .image_container').css('opacity', 1);
+      });
+	  // invoke resrc      
+      resrc.resrc($('.tb-trailpopup .scale'));                      
+    },
+    hide: function(){
+      if (this.popup) {
+     	this.options.map.closePopup(this.popup);
+      }
+    },
+    render: function(){
+      var self = this;
+
+      this.popupContainer = $('<div />');      
+      this.popupContainer.on('click', '.btnView', function(evt) {
+        // fire event          
+        app.dispatcher.trigger("MapTrailDetail:click", this);                        
+      });
+        
+	  if (this.model.get('category') == undefined) {
+		this.model.set('category', '');
+      }
+
+	  if (this.model.get('length')) {
+	    this.model.set('length_km', Math.ceil(this.model.get('length') / 1000));
+	    this.model.set('ascent_m', formatAltitude(Math.round(this.model.get('tags').ascent)));
+	    this.model.set('descent_m', formatAltitude(Math.round(this.model.get('tags').descent)));	  	
+	  }
+
+	  var bEvent = false;
+      switch (this.model.get('slug')) {
+        case '16km':
+        case '30km':
+        case '46km':
+          bEvent = true;	          	
+          this.model.set('sponsorURL', 'event/ultraks');
+          break;	          	  
+        case 'e16':
+        case 'e51':
+          bEvent = true;	          	
+        case 'e101':
+          this.model.set('sponsorURL', 'event/eiger');
+          break;	          	  
+        case 'ttm':
+          bEvent = true;	          	
+          this.model.set('sponsorURL', 'event/tfor');
+          break;	          	  
+        case 'marathon':
+          bEvent = true;	          	
+          this.model.set('sponsorURL', 'event/aom');
+          break;	          	  
+        case 'ultramarathon':
+          bEvent = true;	          	
+          this.model.set('sponsorURL', 'event/laugavegur');
+          break;	          	  
+      	case 'lantau-vertical-hong-kong':
+      	  bEvent = true;	          	
+      	  this.model.set('sponsorURL', 'event/lantauvertical');
+      	  break;	          	  	          	  
+      	case 'heysen-105-south-australia':
+      	  bEvent = true;	          	
+      	  this.model.set('sponsorURL', 'event/heysen105');
+      	  break;	          
+      	case 'scenic-trail-24k-d-2200m-lugano-capriasca-switzerland':
+      	case 'scenic-trail-54k-d-3900m-lugano-capriasca-switzerland':
+      	  bEvent = true;	          	
+      	  this.model.set('sponsorURL', 'event/luganoscenictrail');
+      }
+
+      if (this.model.get('user').type == 'brand') {
+	    this.model.set('sponsorURL', 'profile/' + this.model.get('user').name);
+  	    bEvent = true;
+      }
+
+	  var strPath = '/images/default/example_trailcard.jpg';
+	  if (this.model.get('media')) {
+	    var versions = this.model.get('media').versions;
+	    var strPath = versions[0].path;      	
+	  }
+	  this.model.set('versionLargePath', strPath);      	
+	
+	  if (this.model.get('category') == undefined) {
+		this.model.set('category', '');
+	  }
+	
+	  if (this.model.get('length')) {
+		this.model.set('length_km', Math.ceil(this.model.get('length') / 1000));
+		this.model.set('ascent_m', Math.round(this.model.get('tags').ascent));
+		this.model.set('descent_m', Math.round(this.model.get('tags').descent));	  	
+ 	  }
+
+	  var attribs = this.model.toJSON();
+	    
+      this.template = _.template($('#trailMarkerPopupViewTemplate').text());
+	  if (bEvent) {
+	    this.template = _.template($('#trailEventMarkerPopupViewTemplate').text());	
+	  }
+	    
+      this.popupContainer.html(this.template(attribs));
+                       
+      return this;
+    }
+  });
+
+  return MapTrailMarkerPopup;
+});
