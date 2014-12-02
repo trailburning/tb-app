ec188db7570b4cdb29769b858481d0c21d6d1c34
ptrost <info@cynova.net>
2014-12-02 21:23:57 +0100

    include images in API GET /socialmedia request and format URL's in text

--- src/TB/Bundle/APIBundle/Service/SocialMedia.php
+++ src/TB/Bundle/APIBundle/Service/SocialMedia.php
@@ -29,11 +29,21 @@ class SocialMedia
         if (isset($twitterResult->statuses)) {
             foreach ($twitterResult->statuses as $tweet) {
                 $date = new \DateTime($tweet->created_at);
+                $images = [];
+                if (property_exists($tweet, 'entities') && property_exists($tweet->entities, 'media')) {
+                    foreach ($tweet->entities->media as $media) {
+                        if ($media->type == 'photo') {
+                            $images[] = $media->media_url;
+                        }
+                    }
+                }
+                
                 $result[] = $this->formatItem(
                     $tweet->text, 
                     $date->format('Y-m-d H:i:s'), 
                     $tweet->user->name,
-                    self::TYPE_TWITTER
+                    self::TYPE_TWITTER,
+                    $images
                 );
             }
         }
@@ -41,14 +51,22 @@ class SocialMedia
         return $result;
     }
     
-    protected function formatItem($text, $date, $user, $type) 
+    protected function formatItem($text, $date, $user, $type, array $images) 
     {
         return [
-            'text' => $text,
+            'text' => $this->formatText($text),
             'date' => $date,
             'user' => $user,
             'type' => $type,
+            'images' => $images,
         ];
     }
-    
+        
+    protected function formatText($text) 
+    {
+        // replace URL's by a link tag
+        $text = preg_replace("~((?:http|https|ftp)://(?:\S*?\.\S*?))(?=\s|\;|\)|\]|\[|\{|\}|,|\"|'|:|\<|$|\.\s)~i", '<a href="$1" target="_blank">$1</a>', $text);
+        
+        return $text;
+    }
 }
