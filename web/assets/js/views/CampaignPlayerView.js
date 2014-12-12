0135452b85fb7c0643d94a18ced1e2a804133816
Matt Allbeury <mallbeury@mac.com>
2014-12-12 11:43:10 +0100

    Added campaign tweets, small home page edit.

--- web/assets/js/views/CampaignPlayerView.js
+++ web/assets/js/views/CampaignPlayerView.js
@@ -379,13 +379,14 @@ define([
           
 	  	  var nRouteID = $.cookie('route_id');          
 		  if (nRouteID != undefined) {
-    	    this.trailMapView.setMapView(new L.LatLng($.cookie('route_lat'), $.cookie('route_lng')), $.cookie('route_zoom'));
-            this.trailMapView.selectTrail(nRouteID);
-		  	// remove
-		  	$.removeCookie('route_id');
-		    $.removeCookie('route_lat');
-		    $.removeCookie('route_lng');
-		    $.removeCookie('route_zoom');        
+		  	if (this.trailMapView.selectTrail(nRouteID)) {
+    	      this.trailMapView.setMapView(new L.LatLng($.cookie('route_lat'), $.cookie('route_lng')), $.cookie('route_zoom'));
+		  	}
+	  	    // remove
+	  	    $.removeCookie('route_id');
+	        $.removeCookie('route_lat');
+	        $.removeCookie('route_lng');
+	        $.removeCookie('route_zoom');        
 		  }          
           break;
       }
