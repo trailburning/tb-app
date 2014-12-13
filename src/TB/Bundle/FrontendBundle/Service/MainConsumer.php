050293ea981560fb0c9f50256c0aa0b51b96838d
ptrost <info@cynova.net>
2014-12-13 10:59:58 +0100

    fault tolerant queue consumer commands

--- src/TB/Bundle/FrontendBundle/Service/MainConsumer.php
+++ src/TB/Bundle/FrontendBundle/Service/MainConsumer.php
@@ -110,7 +110,10 @@ class MainConsumer extends Consumer
     {
         $console = realpath(__DIR__ . '/../../../../../app/console');
         $handle = popen(sprintf('php -dmemory_limit=256M %s %s', $console, $command), 'r');
-        $output = fread($handle, 2096);
+        $output = '';
+        while (!feof($handle)) {
+            $output .= fread($handle, 2096);
+        }
         
         // The script outputs 'OK' for success, test only the last 2 characters to handle php error messages and other debug output of the command
         $isSuccess =  (substr(trim($output), -2) == 'OK') ? true : false;
