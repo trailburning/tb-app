bf03fdf750b1ab1b4f52656cc80ca648a53e688d
Matt Allbeury <mallbeury@mac.com>
2014-12-16 20:06:40 +0100

    Updated Twitter url.

--- web/assets/js/views/TwitterView.js
+++ web/assets/js/views/TwitterView.js
@@ -9,13 +9,17 @@ define([
     },            
     getResults: function(){
       var self = this;
+
+	  this.strTwitterURL = 'https://twitter.com/';
 		  
 	  var strURL = TB_RESTAPI_BASEURL + '/v1/socialmedia';
 	  if (this.options.search) {
 		strURL += '?term=' + this.options.search;
+		this.strTwitterURL += 'search?q=' + this.options.search;
 	  }
 	  else if (this.options.user) {
 	    strURL += '?user=' + this.options.user;
+	    this.strTwitterURL += this.options.user;
 	  }
 	  else {
 	  	return;
@@ -50,7 +54,7 @@ define([
       	    	  if (tweet.images.length) {
       	    	    strImage = '<a href="'+tweet.images[0].expanded_url+'" target="_blank"><div class="image_container fade_on_load"><img src="'+tweet.images[0].media_url+'" class="scale" border="0"></div></a>'; 
       	    	  }
-        	      strTweet = '<div class="tweet_panel"><a class="icon" href="https://twitter.com/search?q='+self.options.search+'" target="_blank"></a><div class="panel"><div class="panel_content "><h5 class="tb">'+tweet.text+strImage+'</h5><div class="details"><time class="timeago" datetime="'+tweet.date+'"></time>&nbsp;&nbsp;<strong>'+tweet.user+'</strong></div></div></div></div>';
+        	      strTweet = '<div class="tweet_panel"><a class="icon" href="'+self.strTwitterURL+'" target="_blank"></a><div class="panel"><div class="panel_content "><h5 class="tb">'+tweet.text+strImage+'</h5><div class="details"><time class="timeago" datetime="'+tweet.date+'"></time>&nbsp;&nbsp;<strong>'+tweet.user+'</strong></div></div></div></div>';
       	    	  $(self.el).append(strTweet);
       	    	}
       	    });
