bab2586c49dbf4d812ee2eeb0ce1b2a310d417db
Matt Allbeury <mallbeury@mac.com>
2015-01-06 13:23:56 +0100

    Responsive map.

--- web/demos/responsive/js/views/AppView.js
+++ web/demos/responsive/js/views/AppView.js
@@ -25,6 +25,35 @@ define([
       this.trailSliderView = new TrailSliderView({ el: '.royalSlider', model: this.model, mediaCollection: this.mediaCollection, mediaModel: this.mediaModel });                  
 	  this.trailMapView = new TrailMapView({ el: '#location_map', elCntrls: '#view_map_btns', model: this.model });
 	  	  
+	  this.bFullMap = false
+	  
+	  $('#location_map').addClass('mini');
+	  $('.toggle-btn').click(function(evt){
+	  	switch (self.bFullMap) {
+	  	  case true:
+	  	    self.bFullMap = false;
+	  	    $('#location_map').addClass('mini');
+	  	    $('.royalSlider').removeClass('mini');
+	  	    self.trailSliderView.render();
+//	  	    self.trailMapView.render();
+	  	    self.trailMapView.setView(false);
+	  	    break;
+	  	    
+	  	  case false:
+	  	    self.bFullMap = true;
+	  	    $('#location_map').removeClass('mini');
+	  	    $('.royalSlider').addClass('mini');
+	  	    self.trailSliderView.render();
+//	  	    self.trailMapView.render();
+	  	    self.trailMapView.setView(true);	  	    
+	  	    break;	  		
+	  	}
+	  });	  	  
+	  	  
+      $(window).resize(function() {
+        self.handleResize();
+      });    	  	
+	    
       // get trail    
 //      this.model.set('id', 534);             
       this.model.set('id', 159);             
@@ -38,6 +67,19 @@ define([
           });        
         }      
       });      
+	},	
+	handleResize: function(){
+	  console.log('r:'+$('.location_map').hasClass('mini'));
+	  console.log('t:'+$('.location_map').css('float'));
+	  if ($('.location_map').css('float') == 'none') {
+	  	console.log('MINI');
+	  	this.trailMapView.setView(false);
+	  }
+	  else {
+	  	console.log('FULL');
+	  	this.trailMapView.setView(true);
+	  }
+
 	},
     handleMedia: function(){
       var self = this;
@@ -53,6 +95,8 @@ define([
       });
       this.trailMapView.renderMarkers();
       this.trailSliderView.render();
+      
+      this.handleResize();
 	},
     onTrailSlideChanged: function(nSlide){
       this.trailMapView.gotoMedia(nSlide);
--- web/demos/responsive/js/views/TrailMapView.js
+++ web/demos/responsive/js/views/TrailMapView.js
@@ -11,6 +11,7 @@ define([
     initialize: function(){
       this.elCntrls = this.options.elCntrls;            
       this.bRendered = false;
+      this.bFullView = false;
       this.map = null;
       this.polyline = null;
       this.arrLineCordinates = [];
@@ -154,7 +155,6 @@ define([
       
 	  var marker = L.marker(this.arrLineCordinates[0]).addTo(this.map);			        
       marker.setIcon(L.divIcon({className: 'tb-map-location-marker', html: '<div class="marker"></div>', iconSize: [22, 30], iconAnchor: [11, 30],}));
-	  $(marker._icon).addClass('selected');
     },        
     render: function(){
       if (!this.model) {
@@ -168,18 +168,22 @@ define([
       // already rendered?  Just update
       if (this.bRendered) {
         this.map.invalidateSize();
-        this.map.fitBounds(this.polyline.getBounds(), {padding: [30, 30], animate: false});
-        this.map.zoomOut(1, {animate: false});
+        if (this.bFullView) {
+          this.map.fitBounds(this.red_polyline.getBounds(), {padding: [30, 30], animate: false});
+        }
+        else {
+          this.map.fitBounds(this.white_polyline.getBounds(), {padding: [30, 30], animate: false});        	
+        }
         return;         
       }        
                 
       var self = this;
                         
       this.map = L.mapbox.map(this.el.id, null, {dragging: true, touchZoom: false, scrollWheelZoom:false, doubleClickZoom:false, boxZoom:false, tap:false, zoomControl:false, zoomAnimation:true, attributionControl:false});
-      this.layer_street = L.mapbox.tileLayer('mallbeury.8d4ad8ec');
-//      this.layer_street = L.mapbox.tileLayer('mallbeury.8f5ac718');      
+      this.layer_mini_street = L.mapbox.tileLayer('mallbeury.8f5ac718');     
+      this.layer_full_street = L.mapbox.tileLayer('mallbeury.8d4ad8ec');
       this.layer_sat = L.mapbox.tileLayer('mallbeury.map-eorpnyp3');      
-      this.map.addLayer(this.layer_street);
+      this.map.addLayer(this.layer_mini_street);
 
 	  this.map.on('click', function() {
         for (var nMedia=0; nMedia < self.arrMapMediaViews.length; nMedia++) {
@@ -192,15 +196,25 @@ define([
         self.arrLineCordinates.push([Number(point.coords[1]), Number(point.coords[0])]);        
       });
 
-      var polyline_options = {
+      var white_polyline_options = {
+        color: '#FFF',
+        opacity: 1,
+        weight: 4,
+        clickable: false,
+    	distanceMarkers: { lazy: true }
+      };         
+      this.white_polyline = L.polyline(self.arrLineCordinates, white_polyline_options).addTo(this.map);
+
+      var red_polyline_options = {
         color: '#ed1c24',
         opacity: 1,
         weight: 4,
         clickable: false,
     	distanceMarkers: { lazy: true }
       };         
-      this.polyline = L.polyline(self.arrLineCordinates, polyline_options).addTo(this.map);          
-      this.map.fitBounds(self.polyline.getBounds(), {padding: [30, 30]});
+      this.red_polyline = L.polyline(self.arrLineCordinates, red_polyline_options);
+  
+      this.map.fitBounds(self.white_polyline.getBounds(), {padding: [30, 30]});
                
       this.buildBtns();           
       
@@ -209,7 +223,36 @@ define([
       this.bRendered = true;
                         
       return this;
-    }    
+    },
+    setView: function(bFull){    	
+      if (bFull == this.bFullView) {
+      	console.log('RET');
+      	return;
+      }
+      
+      if (bFull) {
+      	this.bFullView = true;
+      	
+      	this.red_polyline.addTo(this.map);
+      	this.map.removeLayer(this.white_polyline);
+      	
+        this.map.removeLayer(this.layer_mini_street);
+        this.map.addLayer(this.layer_full_street);  
+        this.layer_full_street.redraw();
+      }
+      else {
+      	this.bFullView = false;
+      	
+      	this.white_polyline.addTo(this.map);
+      	this.map.removeLayer(this.red_polyline);
+      	
+        this.map.removeLayer(this.layer_full_street);
+        this.map.addLayer(this.layer_mini_street);  
+        this.layer_mini_street.redraw();      	
+      }
+      this.render();
+    }
+    
   });
 
   return TrailMapView;
